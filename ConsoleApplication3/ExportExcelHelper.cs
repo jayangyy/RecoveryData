db92b00b60f2d7ae6bc6835ab8d868a49faca2b5
yang <361300115@qq.com>
2016-07-19 22:06:23 +0800

    666

---
+++ ConsoleApplication3/ExportExcelHelper.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Web;
+using System.Linq;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.Util;
+
+namespace 网络考试V2
+{
+    public class ExportExcelHelper
+    {
+        public enum NpoiCellFormatType
+        {
+            //百分比
+            PerType=0
+        }
+        /// <summary>
+        /// 导出Excel
+        /// </summary>
+        /// <param name="dataTable">导出数据</param>
+        /// <param name="exportType">导出类型:0 xlsx 其它:xls</param>
+        /// <param name="list">合并单元格数据</param>
+        /// <returns></returns>
+        public static Tuple<string, byte[]> GetMemoryStream(DataTable dataTable, int exportType, List<NPOI.SS.Util.CellRangeAddress> list = null,
+            IEnumerable<string> formatDic =null, IEnumerable<string> ignoredColumns = null, string filter = null)
+        {
+            using (dataTable)
+            {
+                IWorkbook workbook = null;
+                if (exportType == 0)
+                    workbook = new XSSFWorkbook();
+                else
+                    workbook = new HSSFWorkbook();
+
+                ISheet sheet = workbook.CreateSheet();
+                //创建表头
+                IRow headerRow = sheet.CreateRow(0);
+                int index = 0;
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    if (ignoredColumns != null && ignoredColumns.Contains(column.Caption)) continue;
+                    headerRow.CreateCell(index++).SetCellValue(column.Caption);
+                }
+                //创建内容数据行
+                var rows = string.IsNullOrWhiteSpace(filter) ? dataTable.Select() : dataTable.Select(filter);
+                for (int i = 0; i < rows.Length; i++)
+                {
+                    DataRow dataRow = rows[i];
+                    IRow row = sheet.CreateRow(i + 1);
+                    index = 0;
+                    foreach (DataColumn column in dataTable.Columns)
+                    {
+                        var columnName = column.Caption;
+                        if (ignoredColumns != null && ignoredColumns.Contains(columnName)) continue;
+                        var isNumeric = column.DataType.IsValueType;
+                        var cellType = isNumeric ? CellType.Numeric : CellType.String;
+                        var cell = row.CreateCell(index++, cellType);
+                        
+                        cell.CellStyle.VerticalAlignment = VerticalAlignment.Top;
+                        cell.CellStyle.Alignment = HorizontalAlignment.Left;
+                        #region 设置单元格格式
+                        if (formatDic!=null)
+                        {
+                            if(formatDic.Contains(columnName))
+                            {
+                                ICellStyle cellStyle = workbook.CreateCellStyle();
+                                //IDataFormat format = workbook.CreateDataFormat();
+                                //cellStyle.DataFormat = HSSFDataFormat.GetBuiltinFormat("0.00%"); //单元格格式为“0.00”来表示,"￥#,##0"美元显示,"0.00%"百分比显示  
+                                //cell.CellStyle = cellStyle;
+                                //HSSFCellStyle cellStyle = workbook.CreateCellStyle() as HSSFCellStyle;
+                                //HSSFDataFormat format = workbook.CreateDataFormat()  as HSSFDataFormat;
+                                //cellStyle.DataFormat = format.GetFormat("0.00%");
+                                HSSFFont ffont = (HSSFFont)workbook.CreateFont();
+                                //ffont.FontHeight = 12 * 12;
+                                ffont.FontName = "宋体";
+                                ffont.Color = HSSFColor.Black.Index;
+                                cellStyle.SetFont(ffont);
+                                cellStyle.DataFormat = (short)10;
+                                cell.CellStyle = cellStyle;
+                                if(i==1000)
+                                {
+                                    string s = "";
+                                }
+                                
+                            }
+                        }
+                        #endregion
+                        if (isNumeric)
+                        {
+                            if(dataRow[column]==null||string.IsNullOrWhiteSpace(dataRow[column].ToString()))
+                            {
+                                cell.SetCellValue(dataRow[column].ToString());
+                            }
+                            else
+                            {
+                               
+                                if (column.DataType.ToString() == "System.DateTime") {
+                                    cell.SetCellType(CellType.String);
+                                    cell.SetCellValue(dataRow[column].ToString());
+                                } else {
+                                    cell.SetCellValue(Convert.ToDouble(dataRow[column])); }
+                            }
+                        }
+                        else
+                        {
+                            cell.SetCellValue(dataRow[column].ToString());
+                        }
+                    }
+                }
+                if (list != null && list.Count > 0)
+                {
+                    list.ForEach(t => sheet.AddMergedRegion(t));
+                }
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    workbook.Write(ms);
+                    ms.Flush();
+                    return Tuple.Create<string, byte[]>(exportType == 0 ? "xlsx" : "xls", ms.ToArray());
+                }
+
+            }
+        }
+        /// <summary>
+        /// 导出Excel
+        /// </summary>
+        /// <param name="dataTable">导出数据</param>
+        /// <param name="exportType">导出类型:0 xlsx 其它:xls</param>
+        /// <param name="list">合并单元格数据</param>
+        /// <returns></returns>
+        public static Tuple<string, byte[]> GetMemoryStream<T>(List<T> dataTable, int exportType, List<NPOI.SS.Util.CellRangeAddress> list = null, bool careteCaption = false)
+        {
+            return new Tuple<string, byte[]>("r", new byte[] { });
+
+          
+                //IWorkbook workbook = null;
+                //if (exportType == 0)
+                //    workbook = new XSSFWorkbook();
+                //else
+                //    workbook = new HSSFWorkbook();
+
+                //ISheet sheet = workbook.CreateSheet();
+                ////创建表头
+                //IRow headerRow = sheet.CreateRow(0);
+                //foreach (DataColumn column in dataTable.Columns)
+                //{
+                //    headerRow.CreateCell(column.Ordinal).SetCellValue(column.Caption);
+                //}
+                ////创建内容数据行
+                //for (int i = 0; i < dataTable.Rows.Count; i++)
+                //{
+                //    DataRow dataRow = dataTable.Rows[i];
+                //    IRow row = sheet.CreateRow(i + 1);
+
+                //    foreach (DataColumn column in dataTable.Columns)
+                //    {
+                //        var cell = row.CreateCell(column.Ordinal);
+                //        cell.CellStyle.VerticalAlignment = VerticalAlignment.Top;
+                //        cell.CellStyle.Alignment = HorizontalAlignment.Left;
+                //        cell.SetCellValue(dataRow[column].ToString());
+                //    }
+                //}
+                //if (list != null && list.Count > 0)
+                //{
+                //    list.ForEach(t => sheet.AddMergedRegion(t));
+                //}
+                //using (MemoryStream ms = new MemoryStream())
+                //{
+                //    workbook.Write(ms);
+                //    ms.Flush();
+                //    return Tuple.Create<string, byte[]>(exportType == 0 ? "xlsx" : "xls", ms.ToArray());
+                //}
+        }
+        /// <summary>
+        /// 导出Excel
+        /// </summary>
+        /// <param name="dataTable">导出数据</param>
+        /// <param name="exportType">导出类型:0 xlsx 其它:xls</param>
+        /// <param name="list">合并单元格数据</param>
+        /// <returns></returns>
+        public static Tuple<string, byte[]> GetMemoryStream<T>(List<T> dataTable, int exportType, List<NPOI.SS.Util.CellRangeAddress> list = null, string[] captions = null)
+        {
+            return new Tuple<string, byte[]>("r", new byte[] { });
+            //using (dataTable)
+            //{
+            //    IWorkbook workbook = null;
+            //    if (exportType == 0)
+            //        workbook = new XSSFWorkbook();
+            //    else
+            //        workbook = new HSSFWorkbook();
+
+            //    ISheet sheet = workbook.CreateSheet();
+            //    //创建表头
+            //    IRow headerRow = sheet.CreateRow(0);
+            //    foreach (DataColumn column in dataTable.Columns)
+            //    {
+            //        headerRow.CreateCell(column.Ordinal).SetCellValue(column.Caption);
+            //    }
+            //    //创建内容数据行
+            //    for (int i = 0; i < dataTable.Rows.Count; i++)
+            //    {
+            //        DataRow dataRow = dataTable.Rows[i];
+            //        IRow row = sheet.CreateRow(i + 1);
+
+            //        foreach (DataColumn column in dataTable.Columns)
+            //        {
+            //            var cell = row.CreateCell(column.Ordinal);
+            //            cell.CellStyle.VerticalAlignment = VerticalAlignment.Top;
+            //            cell.CellStyle.Alignment = HorizontalAlignment.Left;
+            //            cell.SetCellValue(dataRow[column].ToString());
+            //        }
+            //    }
+            //    if (list != null && list.Count > 0)
+            //    {
+            //        list.ForEach(t => sheet.AddMergedRegion(t));
+            //    }
+            //    using (MemoryStream ms = new MemoryStream())
+            //    {
+            //        workbook.Write(ms);
+            //        ms.Flush();
+            //        return Tuple.Create<string, byte[]>(exportType == 0 ? "xlsx" : "xls", ms.ToArray());
+            //    }
+
+            //}
+
+
+        }
+        public void ImportExtend()
+        {
+
+
+
+            //HSSFWorkbook wk = new HSSFWorkbook(file.InputStream);
+            //ISheet sheet = wk.GetSheetAt(0);
+            //List<Merchandise> list = new List<Merchandise>();
+
+            //for (int i = 1; i <= sheet.LastRowNum; i++)
+            //{
+            //    rowNum = i;
+            //    try
+            //    {
+            //        IRow row = sheet.GetRow(i);
+            //        if (row != null)
+            //        {
+            //            var merchandise = new Merchandise
+            //            {
+            //                Id = Convert.ToInt32(row.GetCell(0).ToString()),
+            //                Title = row.GetCell(2).ToString(),
+            //                Alias = row.GetCell(3).ToString(),
+            //                MerchandiseInfo = new Product
+            //                {
+            //                    Type = new TypeProperty
+            //                    {
+            //                        TypeName = row.GetCell(5).ToString()
+            //                    }
+            //                },
+            //                Price = Convert.ToDecimal(row.GetCell(11).ToString()),
+            //                InventoryQuantity = Convert.ToInt32(row.GetCell(12).ToString())
+            //            };
+            //            list.Add(merchandise);
+            //        }
+            //    }
+
+        }
+
+        //public static void ExportExcel(HttpResponse Response, string filename = "", Tuple<string, byte[]> excel = null)
+        //{
+        //    Response.AddHeader("Content-Disposition", $"attachment; filename={filename}{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.{excel.Item1}");
+        //    Response.ContentType = "application/excel";
+        //    Response.BinaryWrite(excel.Item2);
+        //    Response.Flush();
+        //    Response.Close();
+        //    Response.End();
+        //}
+
+        public static string ExportGridToExcel(DataTable grid, String fileName, List<NPOI.SS.Util.CellRangeAddress> list = null,
+            IEnumerable<string> formatDic = null, IEnumerable<string> ignoredColumns = null, string filter = null)
+        {
+            //try
+            //{
+            //    var ms = ExportExcelHelper.GetMemoryStream(grid, 1, list, formatDic, ignoredColumns, filter);
+            //    ExportExcelHelper.ExportExcel(HttpContext.Current.Response, fileName, ms);
+            //    return null;
+            //}
+            //catch (Exception ex)
+            //{
+            //    return ex.Message;
+            //}
+            return "";
+        }
+
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,54 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db=new Model1())
+            {
+                MyEntity entry = db.MyEntities.FirstOrDefault();
+                ImportExcel(db.Books.ToList());
+            }
+               
+        }
+        private static void ImportExcel(List<教材库> books)
+        {
+            string sql = "INSERT [dbo].[教材库] ([名称], [作者], [单位], [类型], [地址], [知识体系编号], [格式], [电子书编号], [系统编号], [工种], [岗位]) VALUES ";
+            FileStream file = new FileStream(@"E:\Projects\JCK.xlsx", FileMode.Open, FileAccess.Read);
+            XSSFWorkbook wk = new XSSFWorkbook(file);
+            ISheet sheet = wk.GetSheetAt(0);
+            List<string> list = new List<string>();
+            int rowNum = 0;
+            for (int i = 1; i <= sheet.LastRowNum; i++)
+            {
+                rowNum = i;
+                IRow row = sheet.GetRow(i);
+                if (row != null)
+                {
+                   var name=row.GetCell(0).ToString();
+                    var endname = name.Substring(name.IndexOf("_")+1).Trim();
+                    var urlname= name.Substring(name.IndexOf("_") -14).Trim();
+                    if (books.Where(m=>m.地址.Contains(endname.Trim())).Count()==0)
+                    {
+                        list.Add(endname);
+                        var filetype = endname.ToLower().Contains(".pdf") ? "PDF文档" : "视频";
+                        sql += "(N'"+endname.Split('.')[0]+"', N'', N'综合', N'"+ filetype + "', N'http://10.194.5.102/NetExam/upload/"+ urlname + "', -1, N'"+endname.Substring(endname.LastIndexOf('.')+1).ToUpper()+"', NULL, 0, N'测试工种', N'测试岗位'),"+ System.Environment.NewLine;
+                    }
+                }
+
+           }
+            File.WriteAllText(@"e:\1.txt", sql) ;
+
+        }
+    }
+}
